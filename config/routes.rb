cd13e12ad58ddbc7e20a0f3fec894c625d8c6a20
zhaozongchang <xingyingsuifeng@gmail.com>
2018-05-23 17:21:52 +0800

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace :admin do
-   resources :jobs
- end
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
+  end
   devise_for :users
   root 'jobs#index'
 
